c1bec1606fa32c01918c31bfff2eac5c8726d7fa
Richard Wilkes <rich@trollworks.com>
2015-01-04 13:04:39 -0800

    Update copyright for 2015.

--- src/com/trollworks/gcs/character/PageAssembler.java
+++ src/com/trollworks/gcs/character/PageAssembler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998-2014 by Richard A. Wilkes. All rights reserved.
+ * Copyright (c) 1998-2015 by Richard A. Wilkes. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * version 2.0. If a copy of the MPL was not distributed with this file, You
