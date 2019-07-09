8911624f9f54b22b5656bfda00dc05445ed96935
crnormand <crnormand@bellsouth.net>
2019-07-09 09:49:17 -0400

    Converted Skill to use SkillLevel

--- src/com/trollworks/gcs/skill/SkillLevel.java
+++ src/com/trollworks/gcs/skill/SkillLevel.java
@@ -14,13 +14,19 @@ package com.trollworks.gcs.skill;
 /** Provides simple storage for the skill level/relative level pair. */
 public class SkillLevel {
     /** The skill level. */
-    public int mLevel;
+    public int    mLevel;
     /** The relative skill level. */
-    public int mRelativeLevel;
+    public int    mRelativeLevel;
+    /** The tooltip describing how this level was calculated. */
+    public String mToolTip;
+
+    public String getToolTip() {
+        return mToolTip == null ? "" : mToolTip;
+    }
 
     /**
      * Creates a new {@link SkillLevel}.
-     * 
+     *
      * @param level         The skill level.
      * @param relativeLevel The relative skill level.
      */
@@ -28,4 +34,40 @@ public class SkillLevel {
         mLevel         = level;
         mRelativeLevel = relativeLevel;
     }
+
+    /**
+     * Creates a new {@link SkillLevel}.
+     *
+     * @param level         The skill level.
+     * @param relativeLevel The relative skill level.
+     * @param tip           The tooltip to display for this skill.
+     */
+    public SkillLevel(int level, int relativeLevel, String toolTip) {
+        mLevel         = level;
+        mRelativeLevel = relativeLevel;
+        mToolTip       = toolTip;
+    }
+
+    /** @return The level. */
+    public int getLevel() {
+        return mLevel;
+    }
+
+    /** @return The relativeLevel. */
+    public int getRelativeLevel() {
+        return mRelativeLevel;
+    }
+
+    public boolean isDifferentLevelThan(SkillLevel other) {
+        return mLevel != other.mLevel;
+    }
+
+    public boolean isDifferentRelativeLevelThan(SkillLevel other) {
+        return mRelativeLevel != other.mRelativeLevel;
+    }
+
+    public boolean isSameLevelAs(SkillLevel other) {
+        return mLevel == other.mLevel && mRelativeLevel == other.mRelativeLevel;
+    }
+
 }
