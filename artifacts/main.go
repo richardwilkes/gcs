e8e019fb8e5e44fd503f0873a708d98a6b405d5e
Richard Wilkes <wilkes@me.com>
2019-09-07 16:48:52 -0700

    Tweaks for Windows

--- artifacts/main.go
+++ artifacts/main.go
@@ -11,40 +11,37 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "runtime"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
 func main() {
-    exe, err := os.Executable()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    dir := filepath.Dir(exe)
-    var cmdArgs []string
-    if runtime.GOOS == "darwin" {
-        cmdArgs = append(cmdArgs, "-Xdock:icon="+filepath.Join(filepath.Dir(dir), "Resources", "app.png"))
-    }
-    cmdArgs = append(cmdArgs, "-m", "com.trollworks.gcs/com.trollworks.gcs.app.GCS")
-    cmdArgs = append(cmdArgs, os.Args[1:]...)
-    target := filepath.Join(dir, "support", "bin", "gcs")
-    if runtime.GOOS == "windows" {
-        target += ".exe"
-    }
-    cmd := exec.Command(target, cmdArgs...)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if err = cmd.Run(); err != nil {
-        if exitError, ok := err.(*exec.ExitError); ok {
-            os.Exit(exitError.ExitCode())
-        }
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    os.Exit(0)
+	exe, err := os.Executable()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	dir := filepath.Dir(exe)
+	exeName := filepath.Base(exe)
+	var cmdArgs []string
+	if runtime.GOOS == "darwin" {
+		cmdArgs = append(cmdArgs, "-Xdock:icon="+filepath.Join(filepath.Dir(dir), "Resources", "app.png"))
+	}
+	cmdArgs = append(cmdArgs, "-m", "com.trollworks.gcs/com.trollworks.gcs.app.GCS")
+	cmdArgs = append(cmdArgs, os.Args[1:]...)
+	cmd := exec.Command(filepath.Join(dir, "support", "bin", exeName), cmdArgs...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err = cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitError.ExitCode())
+		}
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
