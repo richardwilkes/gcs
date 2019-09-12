8ffaa3f6750f4eb674ba484a49aa4bdbff64048e
Richard Wilkes <wilkes@me.com>
2019-09-11 20:32:29 -0700

    Escape spaces; use tabs in src

--- artifacts/main.go
+++ artifacts/main.go
@@ -12,40 +12,41 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "runtime"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 func main() {
-    exe, err := os.Executable()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    dir := filepath.Dir(exe)
-    exeName := filepath.Base(exe)
-    createLinuxDesktopFile(dir)
-    os.Exit(launchApp(dir, exeName))
+	exe, err := os.Executable()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	dir := filepath.Dir(exe)
+	exeName := filepath.Base(exe)
+	createLinuxDesktopFile(dir)
+	os.Exit(launchApp(dir, exeName))
 }
 
 func createLinuxDesktopFile(dir string) {
-    if runtime.GOOS == "linux" {
-        if home := os.Getenv("HOME"); home != "" {
-            desktopPath := filepath.Join(home, ".local", "share", "applications")
-            if err := os.MkdirAll(desktopPath, 0775); err != nil {
-                fmt.Println(err)
-                return
-            }
-            desktopPath = filepath.Join(desktopPath, "gcs.desktop")
-            f, err := os.Create(desktopPath)
-            if err != nil {
-                fmt.Println(err)
-                return
-            }
-            if _, err = fmt.Fprintf(f, `[Desktop Entry]
+	if runtime.GOOS == "linux" {
+		if home := os.Getenv("HOME"); home != "" {
+			desktopPath := filepath.Join(home, ".local", "share", "applications")
+			if err := os.MkdirAll(desktopPath, 0775); err != nil {
+				fmt.Println(err)
+				return
+			}
+			desktopPath = filepath.Join(desktopPath, "gcs.desktop")
+			f, err := os.Create(desktopPath)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if _, err = fmt.Fprintf(f, `[Desktop Entry]
 Version=1.0
 Type=Application
 Name=GCS
@@ -55,40 +56,40 @@ Exec=%[1]s/gcs %%F
 Categories=Games;Roleplaying
 Keywords=GURPS;GCS;Character;Sheet
 Terminal=false
-`, dir); err != nil {
-                fmt.Println(err)
-            }
-            if cerr := f.Close(); cerr != nil && err == nil {
-                fmt.Println(cerr)
-                return
-            }
-            if err = os.Chmod(desktopPath, 0775); err != nil {
-                fmt.Println(err)
-            }
-        }
-    }
+`, strings.ReplaceAll(dir, " ", `\ `)); err != nil {
+				fmt.Println(err)
+			}
+			if cerr := f.Close(); cerr != nil && err == nil {
+				fmt.Println(cerr)
+				return
+			}
+			if err = os.Chmod(desktopPath, 0775); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
 
 func createCmdArgs(dir string) []string {
-    var cmdArgs []string
-    if runtime.GOOS == "darwin" {
-        cmdArgs = append(cmdArgs, "-Xdock:icon="+filepath.Join(filepath.Dir(dir), "Resources", "app.png"))
-    }
-    cmdArgs = append(cmdArgs, "-m", "com.trollworks.gcs/com.trollworks.gcs.app.GCS")
-    return append(cmdArgs, os.Args[1:]...)
+	var cmdArgs []string
+	if runtime.GOOS == "darwin" {
+		cmdArgs = append(cmdArgs, "-Xdock:icon="+filepath.Join(filepath.Dir(dir), "Resources", "app.png"))
+	}
+	cmdArgs = append(cmdArgs, "-m", "com.trollworks.gcs/com.trollworks.gcs.app.GCS")
+	return append(cmdArgs, os.Args[1:]...)
 }
 
 func launchApp(dir, exeName string) int {
-    cmd := exec.Command(filepath.Join(dir, "support", "bin", exeName), createCmdArgs(dir)...) //nolint:gosec
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if err := cmd.Run(); err != nil {
-        if exitError, ok := err.(*exec.ExitError); ok {
-            return exitError.ExitCode()
-        }
-        fmt.Println(err)
-        return 1
-    }
-    return 0
+	cmd := exec.Command(filepath.Join(dir, "support", "bin", exeName), createCmdArgs(dir)...) //nolint:gosec
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		}
+		fmt.Println(err)
+		return 1
+	}
+	return 0
 }
