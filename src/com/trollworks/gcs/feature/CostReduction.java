9778055f09493be3c1d6d1a764e37dc0becc514d
Richard Wilkes <wilkes@me.com>
2020-01-01 11:18:12 -0800

    Update copyright notices for 2020

--- src/com/trollworks/gcs/feature/CostReduction.java
+++ src/com/trollworks/gcs/feature/CostReduction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998-2019 by Richard A. Wilkes. All rights reserved.
+ * Copyright (c) 1998-2020 by Richard A. Wilkes. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, version 2.0. If a copy of the MPL was not distributed with
