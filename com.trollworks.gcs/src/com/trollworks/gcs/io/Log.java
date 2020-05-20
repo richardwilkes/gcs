bf0575a672ccb422e12050fded3443246c1c2c92
Richard Wilkes <wilkes@me.com>
2020-05-20 09:20:59 -0700

    Removed unused stuff; Change env var to GCS_LOG

--- com.trollworks.gcs/src/com/trollworks/gcs/io/Log.java
+++ com.trollworks.gcs/src/com/trollworks/gcs/io/Log.java
@@ -25,40 +25,13 @@ public class Log {
 
     static {
         OUT = System.out;
-        String property = Debug.getPropertyOrEnvironmentSetting("com.trollworks.log");
+        String property = Debug.getPropertyOrEnvironmentSetting("GCS_LOG");
         if (property != null && !property.isBlank()) {
             try {
                 OUT = new PrintStream(property);
             } catch (Throwable throwable) {
-                error(null, "Unable to redirect log to " + property, throwable);
+                error("Unable to redirect log to " + property, throwable);
             }
-        }
-    }
-
-    /**
-     * Implementors of this interface can be passed to the various logging methods to provide
-     * context.
-     */
-    public interface Context {
-        /** @return A human-readable logging context. */
-        String getLogContext();
-    }
-
-    /**
-     * @param stream The {@link PrintStream} to write the log data to. By default, logging is sent
-     *               to {@link System#out}. Note that when {@link Debug#DEV_MODE} is {@code true},
-     *               calling this method has no effect and logging is always performed to {@link
-     *               System#out}.
-     */
-    public static final void setPrintStream(PrintStream stream) {
-        if (!Debug.DEV_MODE) {
-            if (stream == null) {
-                stream = System.out;
-            }
-            if (OUT != stream && OUT != System.out) {
-                OUT.close();
-            }
-            OUT = stream;
         }
     }
 
@@ -68,7 +41,7 @@ public class Log {
      * @param msg The message to log.
      */
     public static final void error(String msg) {
-        error(null, msg, null);
+        error(msg, null);
     }
 
     /**
@@ -77,7 +50,7 @@ public class Log {
      * @param throwable The {@link Throwable} to log.
      */
     public static final void error(Throwable throwable) {
-        error(null, null, throwable);
+        error(null, throwable);
     }
 
     /**
@@ -87,38 +60,7 @@ public class Log {
      * @param throwable The {@link Throwable} to log.
      */
     public static final void error(String msg, Throwable throwable) {
-        error(null, msg, throwable);
-    }
-
-    /**
-     * Logs an error.
-     *
-     * @param context The {@link Context} this error occurred within.
-     * @param msg     The message to log.
-     */
-    public static final void error(Context context, String msg) {
-        error(context, msg, null);
-    }
-
-    /**
-     * Logs an error.
-     *
-     * @param context   The {@link Context} this error occurred within.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void error(Context context, Throwable throwable) {
-        error(context, null, throwable);
-    }
-
-    /**
-     * Logs an error.
-     *
-     * @param context   The {@link Context} this error occurred within.
-     * @param msg       The message to log.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void error(Context context, String msg, Throwable throwable) {
-        post('E', context, msg, throwable);
+        post('E', msg, throwable);
     }
 
     /**
@@ -127,7 +69,16 @@ public class Log {
      * @param msg The message to log.
      */
     public static final void warn(String msg) {
-        warn(null, msg, null);
+        warn(msg, null);
+    }
+
+    /**
+     * Logs a warning.
+     *
+     * @param throwable The {@link Throwable} to log.
+     */
+    public static final void warn(Throwable throwable) {
+        warn(null, throwable);
     }
 
     /**
@@ -137,118 +88,15 @@ public class Log {
      * @param throwable The {@link Throwable} to log.
      */
     public static final void warn(String msg, Throwable throwable) {
-        warn(null, msg, throwable);
+        post('W', msg, throwable);
     }
 
-    /**
-     * Logs a warning.
-     *
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void warn(Throwable throwable) {
-        warn(null, null, throwable);
-    }
-
-    /**
-     * Logs a warning.
-     *
-     * @param context The {@link Context} this warning occurred within.
-     * @param msg     The message to log.
-     */
-    public static final void warn(Context context, String msg) {
-        warn(context, msg, null);
-    }
-
-    /**
-     * Logs a warning.
-     *
-     * @param context   The {@link Context} this warning occurred within.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void warn(Context context, Throwable throwable) {
-        warn(context, null, throwable);
-    }
-
-    /**
-     * Logs a warning.
-     *
-     * @param context   The {@link Context} this warning occurred within.
-     * @param msg       The message to log.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void warn(Context context, String msg, Throwable throwable) {
-        post('W', context, msg, throwable);
-    }
-
-    /**
-     * Logs an informational message.
-     *
-     * @param msg The message to log.
-     */
-    public static final void info(String msg) {
-        info(null, msg, null);
-    }
-
-    /**
-     * Logs an informational message.
-     *
-     * @param msg       The message to log.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void info(String msg, Throwable throwable) {
-        info(null, msg, throwable);
-    }
-
-    /**
-     * Logs an informational message.
-     *
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void info(Throwable throwable) {
-        info(null, null, throwable);
-    }
-
-    /**
-     * Logs an informational message.
-     *
-     * @param context The {@link Context} this info occurred within.
-     * @param msg     The message to log.
-     */
-    public static final void info(Context context, String msg) {
-        info(context, msg, null);
-    }
-
-    /**
-     * Logs an informational message.
-     *
-     * @param context   The {@link Context} this info occurred within.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void info(Context context, Throwable throwable) {
-        info(context, null, throwable);
-    }
-
-    /**
-     * Logs an informational message.
-     *
-     * @param context   The {@link Context} this info occurred within.
-     * @param msg       The message to log.
-     * @param throwable The {@link Throwable} to log.
-     */
-    public static final void info(Context context, String msg, Throwable throwable) {
-        post('I', context, msg, throwable);
-    }
-
-    private static void post(char levelCode, Context context, String msg, Throwable throwable) {
+    private static void post(char levelCode, String msg, Throwable throwable) {
         StringBuilder buffer = new StringBuilder();
         buffer.append(levelCode);
         buffer.append(SEPARATOR);
         buffer.append(FORMAT.format(new Date()));
         buffer.append(SEPARATOR);
-        if (context != null) {
-            buffer.append(context.getLogContext());
-            buffer.append(SEPARATOR);
-        }
         if (msg != null && !msg.isEmpty()) {
             buffer.append(msg);
         }
