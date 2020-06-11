6b1cd5f79f3f7961271319065105cd9678551ae9
Richard Wilkes <wilkes@me.com>
2020-06-11 10:45:11 -0700

    Bump version to v4.17.5

--- bundler/bundler/Bundler.java
+++ bundler/bundler/Bundler.java
@@ -40,7 +40,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Bundler {
-    private static final String        GCS_VERSION       = "4.17.4";
+    private static final String        GCS_VERSION       = "4.17.5";
     private static       String        JDK_MAJOR_VERSION = "14";
     private static final String        ITEXT_VERSION     = "2.1.7";
     private static final String        LOGGING_VERSION   = "1.2.0";
