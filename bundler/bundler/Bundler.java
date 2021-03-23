2302a612f61190f809f8a012edd4ba13550d7e83
Richard Wilkes <wilkes@me.com>
2021-03-22 19:28:36 -0700

    Bump version to 4.29

--- bundler/bundler/Bundler.java
+++ bundler/bundler/Bundler.java
@@ -40,7 +40,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public final class Bundler {
-    private static final String GCS_VERSION       = "4.28.0";
+    private static final String GCS_VERSION       = "4.29.0";
     private static       String JDK_MAJOR_VERSION = "15";
     private static final String ITEXT_VERSION     = "2.1.7";
     private static final String LINUX             = "linux";
