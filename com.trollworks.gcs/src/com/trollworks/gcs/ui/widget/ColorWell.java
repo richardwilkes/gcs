68a47b9d63b00a361d56dc1a734986a042584d59
Richard Wilkes <wilkes@me.com>
2021-06-29 20:26:04 -0700

    Add keyboard support; focus/rollover cues

--- com.trollworks.gcs/src/com/trollworks/gcs/ui/widget/ColorWell.java
+++ com.trollworks.gcs/src/com/trollworks/gcs/ui/widget/ColorWell.java
@@ -11,20 +11,29 @@
 
 package com.trollworks.gcs.ui.widget;
 
+import com.trollworks.gcs.menu.Command;
+import com.trollworks.gcs.ui.Colors;
+import com.trollworks.gcs.ui.GraphicsUtilities;
 import com.trollworks.gcs.ui.UIUtilities;
-import com.trollworks.gcs.ui.border.LineBorder;
+import com.trollworks.gcs.utility.Geometry;
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JColorChooser;
 
-public class ColorWell extends Panel implements MouseListener {
+public class ColorWell extends Panel implements KeyListener, MouseListener, FocusListener {
     private Color                mColor;
     private ColorChangedListener mListener;
+    private boolean              mRollover;
 
     public ColorWell(Color color, ColorChangedListener listener) {
         this(color, listener, null);
@@ -34,9 +43,34 @@ public class ColorWell extends Panel implements MouseListener {
         mColor = color;
         mListener = listener;
         setToolTipText(tooltip);
-        setBorder(new LineBorder());
-        UIUtilities.setOnlySize(this, new Dimension(22, 22));
+        addKeyListener(this);
         addMouseListener(this);
+        addFocusListener(this);
+        setFocusable(true);
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        if (isMinimumSizeSet()) {
+            return super.getMinimumSize();
+        }
+        return new Dimension(20, 20);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        if (isPreferredSizeSet()) {
+            return super.getPreferredSize();
+        }
+        return new Dimension(20, 20);
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        if (isMaximumSizeSet()) {
+            return super.getMaximumSize();
+        }
+        return new Dimension(20, 20);
     }
 
     public void setColorChangedListener(ColorChangedListener listener) {
@@ -54,26 +88,31 @@ public class ColorWell extends Panel implements MouseListener {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Rectangle bounds = UIUtilities.getLocalInsetBounds(this);
-        g.setColor(Color.WHITE);
-        g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
-        g.setColor(Color.LIGHT_GRAY);
+        Graphics2D gc     = GraphicsUtilities.prepare(g);
+        Rectangle  bounds = UIUtilities.getLocalInsetBounds(this);
+        gc.setColor(Colors.ON_BACKGROUND);
+        gc.fill(bounds);
+        Geometry.inset(1, bounds);
+        gc.setColor((mRollover || isFocusOwner()) ? Colors.SELECTION : Colors.BACKGROUND);
+        gc.fill(bounds);
+        Geometry.inset(1, bounds);
+        gc.setColor(Color.WHITE);
+        gc.fill(bounds);
+        gc.setColor(Color.LIGHT_GRAY);
         int xs     = bounds.width / 4;
         int ys     = bounds.height / 4;
         int offset = 0;
         for (int y = bounds.y; y < bounds.y + bounds.height; y += ys) {
             for (int x = bounds.x + offset; x < bounds.x + bounds.width; x += xs * 2) {
-                g.fillRect(x, y, xs, ys);
+                gc.fillRect(x, y, xs - 1, ys - 1);
             }
             offset = offset == 0 ? xs : 0;
         }
-        g.setColor(mColor);
-        g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+        gc.setColor(mColor);
+        gc.fill(bounds);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent event) {
+    public void click() {
         Color color = JColorChooser.showDialog(this, null, mColor);
         if (color != null) {
             if (!mColor.equals(color)) {
@@ -84,6 +123,11 @@ public class ColorWell extends Panel implements MouseListener {
                 }
             }
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        click();
     }
 
     @Override
@@ -98,11 +142,43 @@ public class ColorWell extends Panel implements MouseListener {
 
     @Override
     public void mouseEntered(MouseEvent event) {
-        // Unused
+        mRollover = true;
+        repaint();
     }
 
     @Override
     public void mouseExited(MouseEvent event) {
+        mRollover = false;
+        repaint();
+    }
+
+    @Override
+    public void focusGained(FocusEvent event) {
+        repaint();
+    }
+
+    @Override
+    public void focusLost(FocusEvent event) {
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent event) {
+        if (!event.isConsumed() && (event.getModifiersEx() & Command.COMMAND_MODIFIER) == 0) {
+            if (event.getKeyChar() == ' ') {
+                event.consume();
+                click();
+            }
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        // Unused
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
         // Unused
     }
 
