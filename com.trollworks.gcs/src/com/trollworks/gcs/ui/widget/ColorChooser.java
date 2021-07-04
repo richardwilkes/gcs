5b4944e24876eeb8acf226b4773d5fe22ea8afd7
Richard Wilkes <wilkes@me.com>
2021-07-04 16:08:32 -0700

    Add additional modes that can be used to specify the color
    - RGB
    - HEX_RGB
    - HSB

--- com.trollworks.gcs/src/com/trollworks/gcs/ui/widget/ColorChooser.java
+++ com.trollworks.gcs/src/com/trollworks/gcs/ui/widget/ColorChooser.java
@@ -12,13 +12,19 @@
 package com.trollworks.gcs.ui.widget;
 
 import com.trollworks.gcs.character.FieldFactory;
+import com.trollworks.gcs.ui.Colors;
+import com.trollworks.gcs.ui.UIUtilities;
 import com.trollworks.gcs.ui.layout.PrecisionLayout;
 import com.trollworks.gcs.ui.layout.PrecisionLayoutData;
 import com.trollworks.gcs.utility.I18n;
+import com.trollworks.gcs.utility.text.DoubleFormatter;
+import com.trollworks.gcs.utility.text.IntegerFormatter;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Window;
 import javax.swing.SwingConstants;
+import javax.swing.text.DefaultFormatterFactory;
 
 public final class ColorChooser extends Panel {
     private ColorWell mOriginal;
@@ -27,13 +33,13 @@ public final class ColorChooser extends Panel {
 
     public static Color presentToUser(Component comp, String title, Color current) {
         ColorChooser cc = new ColorChooser(current);
-        Modal dialog = Modal.prepareToShowMessage(comp, title != null ? title :
+        Modal modal = Modal.prepareToShowMessage(comp, title != null ? title :
                 I18n.text("Choose a color…"), MessageType.QUESTION, cc);
-        dialog.addCancelButton();
-        cc.mSetButton = dialog.addButton(I18n.text("Set"), Modal.OK);
+        modal.addCancelButton();
+        cc.mSetButton = modal.addButton(I18n.text("Set"), Modal.OK);
         cc.adjustButtons();
-        dialog.presentToUser();
-        switch (dialog.getResult()) {
+        modal.presentToUser();
+        switch (modal.getResult()) {
         case Modal.OK:
             Commitable.sendCommitToFocusOwner();
             return cc.mCurrent.getWellColor();
@@ -42,66 +48,158 @@ public final class ColorChooser extends Panel {
         }
     }
 
-    private ColorChooser(Color current) {
-        super(new PrecisionLayout().setColumns(3));
+    private ColorChooser(Color color) {
+        super(new PrecisionLayout().setColumns(2).setMargins(0).setHorizontalSpacing(LayoutConstants.WINDOW_BORDER_INSET));
+        PopupMenu<Choice> popup = new PopupMenu<>(Choice.values(), (p) -> {
+            Choice choice = p.getSelectedItem();
+            if (choice != null) {
+                if (getComponentCount() > 2) {
+                    remove(2);
+                }
+                add(choice.createPanel(this, new Color(mCurrent.getWellColor().getRGB(), true)));
+                invalidate();
+                Window wnd = UIUtilities.getAncestorOfType(this, Window.class);
+                if (wnd != null) {
+                    wnd.setMinimumSize(null);
+                    wnd.setMaximumSize(null);
+                    wnd.setPreferredSize(null);
+                    wnd.pack();
+                }
+            }
+        });
+        popup.setSelectedItem(Choice.RGB, false);
+        add(popup, new PrecisionLayoutData().setHorizontalSpan(2).setMiddleHorizontalAlignment().setBottomMargin(LayoutConstants.TOOLBAR_VERTICAL_INSET));
+        add(createBeforeAfterWells(color));
+        add(Choice.RGB.createPanel(this, new Color(mOriginal.getWellColor().getRGB(), true)));
+    }
 
-        Integer proto = Integer.valueOf(255);
-        current = new Color(current.getRGB(), true);
+    private Panel createBeforeAfterWells(Color color) {
+        Panel panel = new Panel(new PrecisionLayout().setColumns(2).setMargins(0));
+        mCurrent = addWell(panel, I18n.text("Updated"), color);
+        mOriginal = addWell(panel, I18n.text("Original"), color);
+        return panel;
+    }
 
-        add(new Label(I18n.text("Red")), new PrecisionLayoutData().setEndHorizontalAlignment());
-        add(new EditorField(FieldFactory.BYTE, (f2) -> {
-            Color color2 = mCurrent.getWellColor();
-            mCurrent.setWellColor(new Color(((Integer) f2.getValue()).intValue(), color2.getGreen(),
-                    color2.getBlue(), color2.getAlpha()));
-            adjustButtons();
-        }, SwingConstants.RIGHT, Integer.valueOf(current.getRed()), proto,
-                I18n.text("The value of the red channel, from 0 to 255")));
+    private static ColorWell addWell(Panel panel, String title, Color color) {
+        ColorWell well = new ColorWell(new Color(color.getRGB(), true), null);
+        well.setEnabled(false);
+        panel.add(new Label(title), new PrecisionLayoutData().setEndHorizontalAlignment());
+        panel.add(well);
+        return well;
+    }
 
-        mCurrent = new ColorWell(current, null);
-        mCurrent.setEnabled(false);
-        add(mCurrent, new PrecisionLayoutData().setMiddleHorizontalAlignment().
-                setLeftMargin(LayoutConstants.WINDOW_BORDER_INSET));
+    private static void addLabel(Panel panel, String label) {
+        panel.add(new Label(label), new PrecisionLayoutData().setEndHorizontalAlignment());
+    }
 
-        add(new Label(I18n.text("Green")), new PrecisionLayoutData().setEndHorizontalAlignment());
-        add(new EditorField(FieldFactory.BYTE, (f1) -> {
-            Color color1 = mCurrent.getWellColor();
-            mCurrent.setWellColor(new Color(color1.getRed(), ((Integer) f1.getValue()).intValue(),
-                    color1.getBlue(), color1.getAlpha()));
-            adjustButtons();
-        }, SwingConstants.RIGHT, Integer.valueOf(current.getGreen()), proto,
-                I18n.text("The value of the green channel, from 0 to 255")));
-
-        add(new Label(I18n.text("New")), new PrecisionLayoutData().setMiddleHorizontalAlignment().
-                setLeftMargin(LayoutConstants.WINDOW_BORDER_INSET));
-
-        add(new Label(I18n.text("Blue")), new PrecisionLayoutData().setEndHorizontalAlignment());
-        add(new EditorField(FieldFactory.BYTE, (f1) -> {
-            Color color1 = mCurrent.getWellColor();
-            mCurrent.setWellColor(new Color(color1.getRed(), color1.getGreen(),
-                    ((Integer) f1.getValue()).intValue(), color1.getAlpha()));
-            adjustButtons();
-        }, SwingConstants.RIGHT, Integer.valueOf(current.getBlue()), proto,
-                I18n.text("The value of the blue channel, from 0 to 255")));
-
-        mOriginal = new ColorWell(current, null);
-        mOriginal.setEnabled(false);
-        add(mOriginal, new PrecisionLayoutData().setMiddleHorizontalAlignment().
-                setLeftMargin(LayoutConstants.WINDOW_BORDER_INSET));
-
-        add(new Label(I18n.text("Alpha")), new PrecisionLayoutData().setEndHorizontalAlignment());
-        add(new EditorField(FieldFactory.BYTE, (f) -> {
-            Color color = mCurrent.getWellColor();
-            mCurrent.setWellColor(new Color(color.getRed(), color.getGreen(),
-                    color.getBlue(), ((Integer) f.getValue()).intValue()));
-            adjustButtons();
-        }, SwingConstants.RIGHT, Integer.valueOf(current.getAlpha()), proto,
-                I18n.text("The value of the alpha channel, from 0 to 255")));
-
-        add(new Label(I18n.text("Original")), new PrecisionLayoutData().
-                setMiddleHorizontalAlignment().setLeftMargin(LayoutConstants.WINDOW_BORDER_INSET));
+    private interface ColorAdjuster {
+        Color adjustColor(Color color, int value);
     }
 
     private void adjustButtons() {
         mSetButton.setEnabled(mOriginal.getWellColor().getRGB() != mCurrent.getWellColor().getRGB());
+    }
+
+    private enum Choice {
+        RGB {
+            @Override
+            public String toString() {
+                return I18n.text("RGB");
+            }
+
+            Panel createPanel(ColorChooser chooser, Color color) {
+                Panel panel = new Panel(new PrecisionLayout().setColumns(2).setMargins(0));
+                addChannelField(chooser, panel, I18n.text("Red"), color.getRed(),
+                        I18n.text("The value of the red channel, from 0 to 255"),
+                        (c, v) -> new Color(v, c.getGreen(), c.getBlue(), c.getAlpha()));
+                addChannelField(chooser, panel, I18n.text("Green"), color.getGreen(),
+                        I18n.text("The value of the green channel, from 0 to 255"),
+                        (c, v) -> new Color(c.getRed(), v, c.getBlue(), c.getAlpha()));
+                addChannelField(chooser, panel, I18n.text("Blue"), color.getBlue(),
+                        I18n.text("The value of the blue channel, from 0 to 255"),
+                        (c, v) -> new Color(c.getRed(), c.getGreen(), v, c.getAlpha()));
+                addChannelField(chooser, panel, I18n.text("Alpha"), color.getAlpha(),
+                        I18n.text("The value of the alpha channel, from 0 to 255"),
+                        (c, v) -> new Color(c.getRed(), c.getGreen(), c.getBlue(), v));
+                return panel;
+            }
+        },
+        HEX_RGB {
+            @Override
+            public String toString() {
+                return I18n.text("Hex RGB");
+            }
+
+            Panel createPanel(ColorChooser chooser, Color color) {
+                Panel panel = new Panel(new PrecisionLayout().setColumns(2).setMargins(0));
+                addLabel(panel, toString());
+                panel.add(new EditorField(FieldFactory.STRING, (f) -> {
+                    chooser.mCurrent.setWellColor(Colors.decode(f.getText()));
+                    f.setText(Colors.encodeToHex(chooser.mCurrent.getWellColor()));
+                    chooser.adjustButtons();
+                }, SwingConstants.LEFT, Colors.encodeToHex(color), "#FFFFFFFFF",
+                        I18n.text("The color encoded as either a 6 or 8 digit hex value")));
+                return panel;
+            }
+        },
+        HSB {
+            @Override
+            public String toString() {
+                return I18n.text("HSB");
+            }
+
+            Panel createPanel(ColorChooser chooser, Color color) {
+                Panel   panel = new Panel(new PrecisionLayout().setColumns(2).setMargins(0));
+                float[] hsb   = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
+                addLabel(panel, I18n.text("Hue"));
+                panel.add(new EditorField(new DefaultFormatterFactory(new IntegerFormatter(0, 360, false)),
+                        (f) -> {
+                            float hue    = (((Integer) f.getValue()).intValue()) / 360.0f;
+                            float sat    = ((Double) ((EditorField) panel.getComponent(3)).getValue()).floatValue();
+                            float bright = ((Double) ((EditorField) panel.getComponent(5)).getValue()).floatValue();
+                            chooser.mCurrent.setWellColor(new Color(Color.HSBtoRGB(hue, sat, bright) |
+                                    (chooser.mCurrent.getWellColor().getAlpha() << 24), true));
+                            chooser.adjustButtons();
+                        }, SwingConstants.LEFT, Integer.valueOf((int) (hsb[0] * 360)),
+                        Integer.valueOf(360),
+                        I18n.text("The value to use for the hue, from 0 to 360")));
+                DefaultFormatterFactory ff = new DefaultFormatterFactory(new DoubleFormatter(0, 1, false));
+                addLabel(panel, I18n.text("Saturation"));
+                panel.add(new EditorField(ff, (f) -> {
+                    float hue    = (((Integer) ((EditorField) panel.getComponent(1)).getValue()).intValue()) / 360.0f;
+                    float sat    = ((Double) f.getValue()).floatValue();
+                    float bright = ((Double) ((EditorField) panel.getComponent(5)).getValue()).floatValue();
+                    chooser.mCurrent.setWellColor(new Color(Color.HSBtoRGB(hue, sat, bright) |
+                            (chooser.mCurrent.getWellColor().getAlpha() << 24), true));
+                    chooser.adjustButtons();
+                }, SwingConstants.LEFT, Double.valueOf(hsb[1]), Double.valueOf(0.99999),
+                        I18n.text("The value to use for the saturation, from 0 to 1")));
+                addLabel(panel, I18n.text("Brightness"));
+                panel.add(new EditorField(ff, (f) -> {
+                    float hue    = (((Integer) ((EditorField) panel.getComponent(1)).getValue()).intValue()) / 360.0f;
+                    float sat    = ((Double) ((EditorField) panel.getComponent(3)).getValue()).floatValue();
+                    float bright = ((Double) f.getValue()).floatValue();
+                    chooser.mCurrent.setWellColor(new Color(Color.HSBtoRGB(hue, sat, bright) |
+                            (chooser.mCurrent.getWellColor().getAlpha() << 24), true));
+                    chooser.adjustButtons();
+                }, SwingConstants.LEFT, Double.valueOf(hsb[2]), Double.valueOf(0.99999),
+                        I18n.text("The value to use for the brightness, from 0 to 1")));
+                addChannelField(chooser, panel, I18n.text("Alpha"), color.getAlpha(),
+                        I18n.text("The value of the alpha channel, from 0 to 255"),
+                        (c, v) -> new Color(c.getRed(), c.getGreen(), c.getBlue(), v));
+                return panel;
+            }
+        };
+
+        abstract Panel createPanel(ColorChooser chooser, Color color);
+
+        void addChannelField(ColorChooser chooser, Panel panel, String label, int value, String tooltip, ColorAdjuster adjuster) {
+            addLabel(panel, label);
+            panel.add(new EditorField(FieldFactory.BYTE, (f) -> {
+                chooser.mCurrent.setWellColor(adjuster.adjustColor(chooser.mCurrent.getWellColor(),
+                        ((Integer) f.getValue()).intValue()));
+                chooser.adjustButtons();
+            }, SwingConstants.LEFT, Integer.valueOf(value), Integer.valueOf(255), tooltip));
+        }
     }
 }
