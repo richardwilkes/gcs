8d4cc1437b6c0e4793f26600f370bd8d3b55e28c
Richard Wilkes <wilkes@me.com>
2021-07-09 11:05:19 -0700

    Fix NPE when attempting to set a portrait when none already existed

--- com.trollworks.gcs/src/com/trollworks/gcs/character/GURPSCharacter.java
+++ com.trollworks.gcs/src/com/trollworks/gcs/character/GURPSCharacter.java
@@ -67,6 +67,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 /** A GURPS character. */
@@ -1369,7 +1370,7 @@ public class GURPSCharacter extends CollectedModels implements VariableResolver 
      */
     public void postUndoEdit(String name, CharacterSetter setter, Object before, Object after) {
         StdUndoManager mgr = getUndoManager();
-        if (!mgr.isInTransaction() && !before.equals(after)) {
+        if (!mgr.isInTransaction() && !Objects.equals(before, after)) {
             addEdit(new CharacterUndo(this, name, setter, before, after));
         }
     }
