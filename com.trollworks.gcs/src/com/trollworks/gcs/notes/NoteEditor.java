c0006f3aab50786f232556a60367b8ca014c9769
Richard Wilkes <wilkes@me.com>
2022-01-02 16:20:50 -0800

    Update copyright

--- com.trollworks.gcs/src/com/trollworks/gcs/notes/NoteEditor.java
+++ com.trollworks.gcs/src/com/trollworks/gcs/notes/NoteEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ©1998-2021 by Richard A. Wilkes. All rights reserved.
+ * Copyright ©1998-2022 by Richard A. Wilkes. All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, version 2.0. If a copy of the MPL was not distributed with
