26f75a00c85c0ee9cfb83c7c0594d5247051fc64
Richard Wilkes <wilkes@me.com>
2022-09-25 20:53:57 -0700

    Show the settings path in the --help output

--- main.go
+++ main.go
@@ -12,6 +12,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/richardwilkes/gcs/v5/dbg"
 	"github.com/richardwilkes/gcs/v5/model/convert"
 	"github.com/richardwilkes/gcs/v5/model/export"
@@ -39,7 +41,14 @@ func main() {
 	cl.NewGeneralOption(&convertFiles).SetName("convert").SetSingle('c').
 		SetUsage(i18n.Text("Converts all files specified on the command line to the current data format. If a directory is specified, it will be traversed recursively and all files found will be converted. This operation is intended to easily bring files up tot he current version's data format. After all files have been processed, GCS will exit"))
 	cl.NewGeneralOption(&dbg.VariableResolver).SetName("debug-variable-resolver")
+	showSettings := true
+	atexit.Register(func() {
+		if showSettings {
+			fmt.Printf(i18n.Text("\nSettings path: \"%s\"\n"), settings.Path())
+		}
+	})
 	fileList := jotrotate.ParseAndSetup(cl)
+	showSettings = false
 	setup.Setup()
 	settings.Global() // Here to force early initialization
 	switch {
