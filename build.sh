4da7b889207453f6032663809d3d870bee009fcf
Richard Wilkes <wilkes@me.com>
2023-02-12 11:03:57 -0800

    Update golangci-lint v1.51.1

--- build.sh
+++ build.sh
@@ -100,7 +100,7 @@ fi
 
 # Run the linters
 if [ "$LINT"x == "1x" ]; then
-  GOLANGCI_LINT_VERSION=1.50.1
+  GOLANGCI_LINT_VERSION=1.51.1
   TOOLS_DIR=$PWD/tools
   mkdir -p "$TOOLS_DIR"
   if [ ! -e "$TOOLS_DIR/golangci-lint" ] || [ "$("$TOOLS_DIR/golangci-lint" version 2>&1 | awk '{ print $4 }' || true)x" != "${GOLANGCI_LINT_VERSION}x" ]; then
