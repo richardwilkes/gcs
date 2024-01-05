c325c58aa4aa1f3d02df3a6e91ee4e2994bbfa94
Richard Wilkes <wilkes@me.com>
2024-01-05 12:37:30 -0800

    Bump to v5.20.3

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ for arg in "$@"; do
   --dist|-d)
     EXTRA_BUILD_FLAGS="-a -trimpath"
     EXTRA_LD_FLAGS="-s -w"
-    RELEASE="5.20.2"
+    RELEASE="5.20.3"
     DIST=1
     case $(uname -s) in
     Darwin*)
