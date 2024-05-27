f55912fc8e09f6877220357b90aff33ad0c26642
Richard Wilkes <wilkes@me.com>
2024-05-27 14:31:27 -0700

    Update copyright notices

--- server/frontend/src/App.svelte
+++ server/frontend/src/App.svelte
@@ -1,3 +1,14 @@
+<!--
+  - Copyright (c) 1998-2024 by Richard A. Wilkes. All rights reserved.
+  -
+  - This Source Code Form is subject to the terms of the Mozilla Public
+  - License, version 2.0. If a copy of the MPL was not distributed with
+  - this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+  -
+  - This Source Code Form is "Incompatible With Secondary Licenses", as
+  - defined by the Mozilla Public License, version 2.0.
+  -->
+
 <script lang="ts">
 	import { checkSession } from '$lib/session.ts';
 	import { saveSheet, sheet } from '$lib/sheet.ts';
--- server/frontend/src/lib/sheets/personal/Identity.svelte
+++ server/frontend/src/lib/sheets/personal/Identity.svelte
@@ -1,5 +1,5 @@
 <!--
-  - Copyright ©1998-2024 by Richard A. Wilkes. All rights reserved.
+  - Copyright (c) 1998-2024 by Richard A. Wilkes. All rights reserved.
   -
   - This Source Code Form is subject to the terms of the Mozilla Public
   - License, version 2.0. If a copy of the MPL was not distributed with
@@ -8,6 +8,7 @@
   - This Source Code Form is "Incompatible With Secondary Licenses", as
   - defined by the Mozilla Public License, version 2.0.
   -->
+
 <script lang="ts">
 	import Header from '$lib/sheets/widget/Header.svelte';
 	import Label from '$lib/sheets/widget/Label.svelte';
