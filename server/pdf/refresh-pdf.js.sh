031307a19af2da53d858cc9958c7a15d10b0621b
Richard Wilkes <wilkes@me.com>
2024-06-01 16:29:28 -0700

    Fix download of pdfjs for Windows

--- server/pdf/refresh-pdf.js.sh
+++ server/pdf/refresh-pdf.js.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 trap 'echo -e "\033[33;5mBuild failed on build.sh:$LINENO\033[0m"' ERR
 
 VERSION="4.3.136"
+ARCHIVE="pdfjs-${VERSION}-dist.zip"
 
 /bin/rm -rf build web LICENSE pdf.js-version-*
 
-curl -s -L https://github.com/mozilla/pdf.js/releases/download/v${VERSION}/pdfjs-${VERSION}-dist.zip | tar xz
-/bin/rm -f build/*.map web/*.map web/*.pdf web/debugger.*
+curl -s -L https://github.com/mozilla/pdf.js/releases/download/v${VERSION}/${ARCHIVE} -o ${ARCHIVE}
+unzip -qq ${ARCHIVE}
+/bin/rm -f build/*.map web/*.map web/*.pdf web/debugger.* ${ARCHIVE}
 
 touch pdf.js-version-${VERSION}
