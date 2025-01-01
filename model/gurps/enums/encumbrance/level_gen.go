deca3539826031b653b91a4cc75a36172e9f7a3e
Richard Wilkes <wilkes@me.com>
2025-01-01 15:21:05 -0800

    Bump copyright year

--- model/gurps/enums/encumbrance/level_gen.go
+++ model/gurps/enums/encumbrance/level_gen.go
@@ -1,6 +1,6 @@
 // Code generated from "enum.go.tmpl" - DO NOT EDIT.
 
-// Copyright (c) 1998-2024 by Richard A. Wilkes. All rights reserved.
+// Copyright (c) 1998-2025 by Richard A. Wilkes. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, version 2.0. If a copy of the MPL was not distributed with
--- server/frontend/src/lib/sheets/attributes/BodyType.svelte
+++ server/frontend/src/lib/sheets/attributes/BodyType.svelte
@@ -1,5 +1,5 @@
 <!--
-  - Copyright (c) 1998-2024 by Richard A. Wilkes. All rights reserved.
+  - Copyright (c) 1998-2025 by Richard A. Wilkes. All rights reserved.
   -
   - This Source Code Form is subject to the terms of the Mozilla Public
   - License, version 2.0. If a copy of the MPL was not distributed with
