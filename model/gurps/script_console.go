04b97695d22939b119fa45491e63a44dd0561b12
Richard Wilkes <wilkes@me.com>
2025-05-26 13:02:11 -0700

    Just provide console.log()

--- model/gurps/script_console.go
+++ model/gurps/script_console.go
@@ -9,26 +9,6 @@ import (
 type scriptConsole struct{}
 
 func (c *scriptConsole) Log(args ...any) {
-	slog.Info(c.format(args...))
-}
-
-func (c *scriptConsole) Error(args ...any) {
-	slog.Error(c.format(args...))
-}
-
-func (c *scriptConsole) Warn(args ...any) {
-	slog.Warn(c.format(args...))
-}
-
-func (c *scriptConsole) Info(args ...any) {
-	slog.Info(c.format(args...))
-}
-
-func (c *scriptConsole) Debug(args ...any) {
-	slog.Debug(c.format(args...))
-}
-
-func (c *scriptConsole) format(args ...any) string {
 	var buffer strings.Builder
 	for argNum, arg := range args {
 		if argNum > 0 {
@@ -36,5 +16,5 @@ func (c *scriptConsole) format(args ...any) string {
 		}
 		fmt.Fprintf(&buffer, "%v", arg)
 	}
-	return buffer.String()
+	slog.Info(buffer.String())
 }
