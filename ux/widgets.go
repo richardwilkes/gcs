3cf838db857c2642efc7992d72e1d26df990fd36
Mikolaj Tomczynski <mikolaj@tomczynski.org>
2026-05-12 12:32:32 +0100

    lang: changed label for "Specialization" to "Required Specialization"

--- ux/widgets.go
+++ ux/widgets.go
@@ -94,7 +94,7 @@ func addNameLabelAndField(parent *unison.Panel, fieldData *string) {
 }
 
 func addSpecializationLabelAndField(parent *unison.Panel, fieldData *string) {
-	addLabelAndStringField(parent, i18n.Text("Specialization"), "", fieldData)
+	addLabelAndStringField(parent, i18n.Text("Required Specialization"), "", fieldData)
 }
 
 func addOptionalSpecializationLabelAndField(parent *unison.Panel, fieldData *string) {
